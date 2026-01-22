e0df8a3cd66e51dab6a7a2926bbe6384a0980134
Edwin <edwinw6796@gmail.com>
2026-01-22 10:17:22 +0000

    Adminlogin pagina page + test toegevoegd

---
+++ tests/pages/AdminLoginPage.ts
@@ -0,0 +1,47 @@
+import { Page, Locator, expect } from '@playwright/test';
+import { BasePage } from './BasePage';
+import { getSystemErrorMessage } from 'node:util';
+
+export class AdminLoginPage extends BasePage {
+
+    //navigatie elementen
+
+    readonly usernameInput: Locator;
+    readonly passwordInput: Locator;
+    readonly loginButton: Locator;
+    readonly loginMessage: Locator;
+
+    constructor(page: Page) {
+        super(page)
+
+
+        //de velden:
+        this.usernameInput = page.locator('#username');
+        this.passwordInput = page.locator('#password');
+        this.loginButton = page.locator('button[type="submit"]');
+        this.loginMessage = page.locator('#login-message');
+
+    }
+
+    async open() {
+        await this.goto('/admin-login.html');
+        await expect(this.usernameInput).toBeVisible();
+    }
+
+    async login(username: string, password: string) {
+        await this.usernameInput.fill(username);
+        await this.passwordInput.fill(password);
+        await this.loginButton.click();
+
+    }
+
+    async getMessage() {
+        return this.loginMessage.textContent();
+    }
+
+
+
+
+
+
+}
---
+++ tests/specs/admin-login.spec.ts
@@ -0,0 +1,27 @@
+import {test, expect} from '@playwright/test';
+import { AdminLoginPage } from '../pages/AdminLoginPage';
+
+test('Admin kan succesvol inloggen', async({page}) => {
+
+    const loginPage = new AdminLoginPage(page);
+    await loginPage.open();
+    await loginPage.login('admin','supersecret');
+
+    
+    await expect(loginPage.loginMessage).not.toBeVisible();
+
+
+})
+
+test('Foutieve inlog wordt geweigerd', async({page}) => {
+
+    const loginPage = new AdminLoginPage(page);
+    await loginPage.open();
+    await loginPage.login('admin','admin');
+
+    
+    await expect(loginPage.loginMessage).toHaveText('Invalid username or password');
+
+
+})
+
